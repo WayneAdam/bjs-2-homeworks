562ac437f4200ee3e2969d039a3f221d4cf12bcf
Wayne <meibeat@mail.ru>
2023-06-18 18:29:34 +0300

    Clean correct

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -117,5 +117,3 @@ function makeWork(arrOfArr, func) {
   //возврат максимального значения результатов насадки
   return maxWorkerResult;
 };
-const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
-console.log(makeWork(arr, differenceMaxMinWorker));
